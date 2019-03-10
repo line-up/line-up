77f05561036395160a98ecc98bddd9b9e5cc1511
Ben Adam <bxadam@godaddy.com>
2019-03-09 22:29:58 -0700

    move docs to Parcel

--- docs/babel.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  'presets': [ 'linaria/babel', '@babel/preset-flow', '@babel/preset-env', '@babel/preset-react' ],
-  'plugins': [ '@babel/plugin-proposal-class-properties'],
-}
