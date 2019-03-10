a0a833559a967cf9752a849f5a793f2fb4750a83
Ben Adam <bxadam@godaddy.com>
2019-03-09 22:30:20 -0700

    Move to Emotion temporarily

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-  'presets': [ 'linaria/babel', '@babel/preset-flow', '@babel/preset-env', '@babel/preset-react' ],
-  'plugins': [ '@babel/plugin-proposal-class-properties'],
-}
+  presets: [
+    "linaria/babel",
+    "@babel/preset-flow",
+    "@babel/preset-env",
+    "@babel/preset-react"
+  ],
+  plugins: ["@babel/plugin-proposal-class-properties"]
+};
