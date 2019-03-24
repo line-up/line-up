1cb20ed56777fa4eafa3256db00ba9dfe752a7bd
benadam11 <benjamin.mark.adam@gmail.com>
2019-03-24 13:21:43 -0700

    Show grid in docs

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,30 +1,37 @@
-import React, { Fragment, useState } from 'react';
-import { render } from 'react-dom';
+import React, { Fragment, useState } from "react";
+import { render } from "react-dom";
 
 // TODO: Fix workspace link
-import { Button } from '../../packages/web';
-import { ThemeProvider, dark, light } from '../../packages/themes';
+import { Button, Grid } from "../../packages/web";
+import { ThemeProvider, dark, light } from "../../packages/themes";
 
-const themeMap = {dark, light};
+const themeMap = { dark, light };
 
 function App() {
-  const [ theme, setTheme ] = useState('light');
+  const [theme, setTheme] = useState("light");
   return (
     <Fragment>
       <p>
         Theme Select:
         <select onChange={ev => setTheme(ev.target.value)}>
-          <option value='light'>Light</option>
-          <option value='dark'>Dark</option>
+          <option value="light">Light</option>
+          <option value="dark">Dark</option>
         </select>
       </p>
-      <div className='container'>
+      <div className="container">
         <ThemeProvider theme={themeMap[theme]}>
-          <Button>My Great Button</Button>
+          <Grid>
+            <Button>My Great Button</Button>
+            <Button>My Great Button</Button>
+            <Button>My Great Button</Button>
+            <Button>My Great Button</Button>
+            <Button>My Great Button</Button>
+            <Button>My Great Button</Button>
+          </Grid>
         </ThemeProvider>
       </div>
     </Fragment>
   );
 }
 
-render(<App/>, document.querySelector('#root'));
+render(<App />, document.querySelector("#root"));
