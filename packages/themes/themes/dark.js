ec1e1d24f3c017006edc676f9f11e480c988b8af
benadam11 <benjamin.mark.adam@gmail.com>
2019-03-24 13:20:06 -0700

    add base to light / dark themes

--- packages/themes/themes/dark.js
+++ packages/themes/themes/dark.js
@@ -1,8 +1,11 @@
+import * as baseTheme from "./base";
+
 const darkTheme = {
   colors: {
-    primary: 'white',
-    secondary: 'black'
-  }
+    primary: "white",
+    secondary: "black"
+  },
+  ...baseTheme
 };
 
 export default darkTheme;
--- packages/themes/themes/light.js
+++ packages/themes/themes/light.js
@@ -1,8 +1,10 @@
+import * as baseTheme from "./base";
 const lightTheme = {
   colors: {
-    primary: 'black',
-    secondary: '#f9f9f9'
-  }
+    primary: "black",
+    secondary: "#f9f9f9"
+  },
+  ...baseTheme
 };
 
 export default lightTheme;
