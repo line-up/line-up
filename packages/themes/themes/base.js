d466fac7188421761a1fa659b4bdb0fe2a01b4f4
benadam11 <benjamin.mark.adam@gmail.com>
2019-03-30 21:58:39 -0700

    Update base theme to work w/ styled system

--- packages/themes/themes/base.js
+++ packages/themes/themes/base.js
@@ -3,14 +3,14 @@ export const lineHeight = fontSize * 1.5;
 export const baseLine = lineHeight / 2;
 export const blockSpacingTop = 0;
 export const blockSpacingBottom = lineHeight * 1;
-export const hrThickness = 2;
 export const columnGap = lineHeight;
 export const rowGap = lineHeight;
-export const displayScale = 1;
-export const pixel = 1;
-export const breakpoints = [
-  "@media (min-width: 768px)",
-  "@media (min-width: 1024px)",
-  "@media (min-width: 1280px)",
-  "@media (min-width: 1536px)"
-];
+export const breakpoints = [768, 1024, 1280, 1536];
+export const space = [0, 2, 4, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88, 96];
+export const colors = {
+  white: "white",
+  blue: "blue",
+  green: "green",
+  red: "red",
+  black: "black"
+};
